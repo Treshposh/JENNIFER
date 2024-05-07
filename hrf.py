68713401115d25c413a04484242c7f6d2505b32d
Treasure <treasureonwuchekwa7@gmail.com>
2024-05-07 05:43:27 -0700

    i added a new file

---
+++ hrf.py
@@ -0,0 +1,16 @@
+
+
+x=2
+y=5
+
+if 1<x<=3 and 4>y>=3:  
+    print("hello world")
+else:
+    print("false")
+    x=1+2**3/2*3
+    print(x)
+    inp=input("Enter Your Name: ")
+    print(inp)
+    inp=input("write your age: ")
+    print(inp)
+    inp
